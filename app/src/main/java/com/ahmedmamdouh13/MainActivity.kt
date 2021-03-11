963e5b5597d8c7fe00b76ebd08da3b46b334e353
ahmedmamdouh13 <ahmedmamdouh13196@gmail.com>
2021-03-11 13:24:51 +0200

    try

--- app/src/main/java/com/ahmedmamdouh13/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh13/MainActivity.kt
@@ -2,27 +2,33 @@ package com.ahmedmamdouh13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.TextView
+import com.ahmedmamdouh13.databinding.ActivityMainBinding
 import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.CameraActivity
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
 
 class MainActivity : CameraActivity() {
+
+
+    private lateinit var binder: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        binder = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binder.root)
 
     }
 
     override fun onResume() {
         super.onResume()
 
-        if (!OpenCVLoader.initDebug()){
+        if (!OpenCVLoader.initDebug()) {
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback)
-        } else{
+        } else {
             baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS)
         }
     }
@@ -42,11 +48,9 @@ class MainActivity : CameraActivity() {
         override fun onManagerConnected(status: Int) {
             super.onManagerConnected(status)
 
-            if (status == SUCCESS){
+            if (status == SUCCESS) {
 
-            }
-            else
-            {
+            } else {
 
             }
 
