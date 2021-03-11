de753495fee15193ada392b4b712e5874fde2426
ahmedmamdouh13 <ahmedmamdouh13196@gmail.com>
2021-03-11 12:57:42 +0200

    fix cmakefile errors. add viewBinding

--- app/src/main/java/com/ahmedmamdouh13/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh13/MainActivity.kt
@@ -3,8 +3,12 @@ package com.ahmedmamdouh13
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import org.opencv.android.BaseLoaderCallback
+import org.opencv.android.CameraActivity
+import org.opencv.android.LoaderCallbackInterface
+import org.opencv.android.OpenCVLoader
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CameraActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,16 +17,39 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+    override fun onResume() {
+        super.onResume()
+
+        if (!OpenCVLoader.initDebug()){
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback)
+        } else{
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS)
+        }
+    }
+
     external fun drawFeatures(matRGB: Long, matGray: Long): String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
+        }
+    }
+
+
+    val baseLoaderCallback = object : BaseLoaderCallback(this) {
+
+        override fun onManagerConnected(status: Int) {
+            super.onManagerConnected(status)
+
+            if (status == SUCCESS){
+
+            }
+            else
+            {
+
+            }
+
         }
     }
 }
